1eaddaac79a06a5d8c80aebb31ec0d8a2ad34c44
fa1c0n1 <tmacfalcon@gmail.com>
2020-05-09 20:19:39 +0800

    修改fastjson反序列化功能演示程序 FastjsonDemo

--- src/main/java/me/mole/fastjson/bean/User.java
+++ src/main/java/me/mole/fastjson/bean/User.java
@@ -1,29 +1,51 @@
 package me.mole.fastjson.bean;
 
-import java.util.Properties;
-
 public class User {
-    private Long   id;
-    private String name;
+    private String name; //私有属性, 有getter、setter方法
+    private int   age; //私有属性，有getter、setter方法
+    private boolean flag; //私有属性，有is、setter方法
+    public String sex; //公有属性，无getter、setter方法
+    private String address; //私有属性, 无getter、setter方法
 
     public User() {
-        System.out.println("User constructor method called.");
+        System.out.println("call User default constructor method.");
     }
 
-    public Long getId() {
-        System.out.println("getId() called");
-        return id;
+    public int getAge() {
+        System.out.println("call User getAge()");
+        return this.age;
     }
-    public void setId(Long id) {
-        System.out.println("setId() called");
-        this.id = id;
+    public void setAge(int age) {
+        System.out.println("call User setAge()");
+        this.age = age;
     }
     public String getName() {
-        System.out.println("getName() called");
-        return name;
+        System.out.println("call User getName()");
+        return this.name;
     }
     public void setName(String name) {
-        System.out.println("setName() called");
+        System.out.println("call User setName()");
         this.name = name;
+    }
+
+    public boolean isFlag() {
+        System.out.println("call User isFlag()");
+        return this.flag;
+    }
+
+    public void setFlag(boolean flag) {
+        System.out.println("call User setFlag()");
+        this.flag = flag;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", flag=" + flag +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
--- src/main/java/me/mole/fastjson/demo/FastjsonDemo.java
+++ src/main/java/me/mole/fastjson/demo/FastjsonDemo.java
@@ -1,16 +1,51 @@
 package me.mole.fastjson.demo;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
 import me.mole.fastjson.bean.User;
 
 public class FastjsonDemo {
+
     public static void main(String[] args) {
-        String jsonStr = "{\"name\":\"guest\",\"id\":12}";
-        System.out.println("deserialize...");
-        User user = JSON.parseObject(jsonStr, User.class);
-        System.out.println("------------------");
-        System.out.println("serialize...");
-        String s = JSON.toJSONString(user);
+        //序列化
+        String jsonStr = "{\"@type\":\"me.mole.fastjson.bean.User\",\"name\":\"lala\",\"age\":11, \"flag\": true,\"sex\":\"boy\",\"address\":\"china\"}";
+        System.out.println("-----------------------------------------------\n");
+
+        //通过parse方法进行反序列化，返回的是Object
+        Object obj1 = JSON.parse(jsonStr);
+        System.out.println("JSON.parse(jsonStr): ");
+        System.out.println("parse反序列化对象名称:" + obj1.getClass().getName());
+        System.out.println("parse反序列化对象：" + obj1);
+        System.out.println("-----------------------------------------------\n");
+
+        //通过parseObject方法进行反序列化，不指定类，返回的是JSONObject
+        Object obj2 = JSON.parseObject(jsonStr);
+        System.out.println("JSON.parseObject(jsonStr): ");
+        System.out.println("parseObject反序列化对象名称:" + obj2.getClass().getName());
+        System.out.println("parseObject反序列化:" + obj2);
+        System.out.println("-----------------------------------------------\n");
+
+        //通过parseObject方法进行序列化，指定类为Object.class
+        Object obj3 = JSON.parseObject(jsonStr, Object.class);
+        System.out.println("JSON.parseObject(jsonStr): ");
+        System.out.println("parseObject反序列化对象名称:" + obj3.getClass().getName());
+        System.out.println("parseObject反序列化:" + obj3);
+        System.out.println("-----------------------------------------------\n");
+
+        //通过parseObject方法进行序列化,指定类为User.class
+        Object obj4 = JSON.parseObject(jsonStr, User.class);
+        System.out.println("JSON.parseObject(jsonStr): ");
+        System.out.println("parseObject反序列化对象名称:" + obj4.getClass().getName());
+        System.out.println("parseObject反序列化:" + obj4);
+        System.out.println("-----------------------------------------------\n");
+
+        //通过parseObject方法进行反序列化，指定类为User.class,并指定SupportNonPublicField
+        // 指定SupportNonPublicField后，即使address是private，也能被成功赋值.
+        Object obj5 = JSON.parseObject(jsonStr, User.class, Feature.SupportNonPublicField);
+        System.out.println("JSON.parseObject(jsonStr): ");
+        System.out.println("parseObject反序列化对象名称:" + obj5.getClass().getName());
+        System.out.println("parseObject反序列化:" + obj5);
+        System.out.println("-----------------------------------------------\n");
     }
 }
 
